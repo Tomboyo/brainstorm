2a60e9c0fe2ad4c9477089ece58ad42f4dd704a1
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2019-11-24 16:19:07 -0500

    Add coverate for Fact.persist

--- services/apps/database/test/database/fact_test.exs
+++ services/apps/database/test/database/fact_test.exs
@@ -1,5 +1,26 @@
 defmodule Database.FactTest do
   use ExUnit.Case
   use Database.Case
+  alias Database.{ Fact, Topic }
   doctest Database.Fact
+
+  def setup_persistent_topic(_context) do
+    topic = Topic.new("my label")
+    :ok = Topic.persist(topic)
+    [ topic: topic ]
+  end
+
+  describe "given a transient fact" do
+    setup :setup_persistent_topic
+    setup %{ topic: topic } do
+      [ fact: Fact.new([ topic.id ], "transient fact content") ]
+    end
+
+    test "Fact.persist returns :ok", %{
+      fact: transient
+    } do
+      assert :ok == Fact.persist(transient)
+    end
+  end
+
 end
