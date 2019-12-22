48dcc0e909edb4cebd09d5a63fd1380cf013f402
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2019-12-21 23:10:50 -0500

    Back-fill another fact presenter test

--- services/apps/rest/test/rest/presenter/fact_test.exs
+++ services/apps/rest/test/rest/presenter/fact_test.exs
@@ -3,11 +3,19 @@ defmodule Rest.Presenter.FactTest do
   alias Rest.Presenter.Fact
 
 
-  describe "POST /" do
+  describe "POST / (when given a fact)" do
     test "json-encodes the given fact" do
       fact = Database.Fact.from(Database.Id.new(), "content", [ "topic" ])
 
       assert { :ok, Jason.encode!(fact) } == Fact.present({ :post, "/" }, fact)
+    end
+  end
+
+  describe "POST / (when given partial topic matches)" do
+    test "json-encodes the partially-matched topics" do
+      matches = %{ "search term" => Database.Topic.new("similar label") }
+      assert { :ok, Jason.encode!(matches) } ==
+        Fact.present({ :post, "/" }, matches)
     end
   end
 end
