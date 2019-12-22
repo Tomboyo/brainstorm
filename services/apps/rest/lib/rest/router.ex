4f1efca9ee48bdd2ac571b884d0852d7847f2952
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2019-12-22 13:55:14 -0500

    Add trivial error handling for debug purposes

--- services/apps/rest/lib/rest/router.ex
+++ services/apps/rest/lib/rest/router.ex
@@ -1,5 +1,7 @@
 defmodule Rest.Router do
+  require Logger
   use Plug.Router
+  use Plug.ErrorHandler
 
   plug :match
   plug :dispatch
@@ -7,5 +9,10 @@ defmodule Rest.Router do
   forward "/topic",    to: Rest.Router.Topic
   forward "/fact",     to: Rest.Router.Fact
   forward "/document", to: Rest.Router.Document
+
+  def handle_errors(conn, error) do
+    Logger.log(:error, IO.inspect(error))
+    send_resp(conn, 500, "Internal server error.")
+  end
 
 end
