dbd418e11f5cabc1671d35c616cf32c29d87df16
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2019-12-26 12:56:29 -0500

    Replace `with` macro with Maybe pipeline.
    - `with` has the difficulty that if several patterns fail to match with
      the same { :error, value } tuple, there isn't a clear way to
      differentiate them in the `else`.
    - Maybe Allows us to decorate "error" evaluations to more easily match
      the end-state of an erroneous pipeline. We can decorate
      failed matches with ad-hoc tags, then pipe the result into a `case`.

--- services/apps/rest/lib/rest/router/document.ex
+++ services/apps/rest/lib/rest/router/document.ex
@@ -2,6 +2,7 @@ defmodule Rest.Router.Document do
   use Plug.Router
   require Logger
   alias Database.Id
+  alias Rest.Util.Maybe
 
   @document_db Application.get_env(
     :rest, :document_database, Database.Document)
@@ -17,16 +18,26 @@ defmodule Rest.Router.Document do
   plug :dispatch
 
   get "/:id" do
-    with { :ok, params }   <- Map.fetch(conn, :params),
-         { :ok, id     }   <- Map.fetch(params, "id"),
-         id                <- @topic_db.resolve_id(id),
-         id                <- Id.from(id),
-         { :ok, document } <- @document_db.fetch(id),
-         { :ok, body }     <- @presenter.present({ :get, "/:id" }, document)
-    do
-      conn
-      |> put_resp_header("content-type", "application/json")
-      |> send_resp(200, body)
+    Maybe.of(fn -> conn end)
+      |> Maybe.map(&Map.fetch(&1, :params), :missing_params)
+      |> Maybe.map(&Map.fetch(&1, "id"),    :missing_id)
+      |> Maybe.replace(&@topic_db.resolve_id(&1))
+      |> Maybe.replace(&Id.from(&1))
+      |> Maybe.map(&@document_db.fetch(&1), :document_error)
+      |> Maybe.map(&@presenter.present({ :get, "/:id" }, &1), :presenter_error)
+      |> Maybe.produce()
+    |> case do
+      { :ok, body } ->
+        conn
+        |> put_resp_header("content-type", "application/json")
+        |> send_resp(200, body)
+      { :error, { :document_error, id, :enoent }} ->
+        conn
+        |> put_resp_header("content-type", "application/json")
+        |> send_resp(404, Jason.encode!(%{
+            "error" => "No document with id `#{id}` was found."
+          }))
+      { :error, any } -> any # unhandled match -- bublle up to the router
     end
   end
 
---
+++ services/apps/rest/lib/rest/util/maybe.ex
@@ -0,0 +1,89 @@
+defmodule Rest.Util.Maybe do
+  @opaque t :: %__MODULE__{}
+  defstruct [ :functions ]
+
+  @doc """
+  Begin a maybe pipeline with the given supplier function. This function will
+  produce the initial state of the Maybe when produce/0 is invoked.
+  """
+  def of(supplier) do
+    %__MODULE__{ functions: [ supplier ]}
+  end
+
+  @doc """
+  Defaults the expected_tag argument of map/4 to :ok. Equivalent to
+  `map(maybe, :ok, function, error_tag)`.
+  """
+  def map(maybe, function, error_tag) do
+    map(maybe, :ok, function, error_tag)
+  end
+
+  @doc """
+  Register a mapping function which will consume the current state of the Maybe
+  and return the new state of the Maybe. All mappings are executed in the order
+  given.
+
+  If the mapping returns a value of the form `{ ^expected_tag, v }`, then the
+  state of the Maybe becomes `v`. Otherwise, the state becomes an error tuple
+  `{ ^error_tag, state, error }`, where `state` is the state of the Maybe given
+  to the failed mapping and `error` is the resolved value of the failed mapping.
+
+  Mappings are applied only when `produce/0` is invoked.
+  """
+  def map(maybe, expected_tag, function, error_tag) do
+    lifted_function = lift_function(expected_tag, function, error_tag)
+    %__MODULE__{ functions: [ lifted_function ] ++ maybe.functions }
+  end
+
+  defp lift_function(expected_tag, function, error_tag) do
+    fn state ->
+      case function.(state) do
+        { ^expected_tag, value } -> { :ok, value }
+        _ -> { :error, error_tag }
+      end
+    end
+  end
+
+  @doc """
+  Like map/4, register a mapping function which will consume the current state
+  of the Maybe and return the new state of the Maybe. Unlike map/4, the function
+  is assumed to never return an error, so the value is taken verbatim as the new
+  state of the Maybe.
+
+  As in map/4, the registered function is executed in the order given once
+  produce/4 is invoked.
+  """
+  def replace(maybe, function) do
+    lifted_function = fn state -> { :ok, function.(state) } end
+
+    %__MODULE__{ functions: [ lifted_function ] ++ maybe.functions }
+  end
+
+  @doc """
+  Apply all registered functions in the order registered. In particular:
+  1. The supplier registered via `of` is applied to obtain the initial Maybe
+     state.
+  2. Each mapping registered via `map` or `replace` is applied to the state in
+     turn, replacing the state. If any mapping fails, evaluation terminates
+     immediately (no other mappings are evaluated).
+  3. The final state is returned. If all mappings resolved normally, the result
+     is an `{ :ok, value }` tuple. If any mapping failed, then the result is the
+     error evaluation of the first such mapping.
+  """
+  def produce(maybe) do
+    [ initial_value | mappings ] = Enum.reverse(maybe.functions)
+    produce(initial_value.(), hd(mappings), tl(mappings))
+  end
+
+  defp produce(state, function, [] = _functions) do
+    # this is in the form { :ok, v }
+    function.(state)
+  end
+
+  defp produce(state, function, functions) do
+    case function.(state) do
+      { :ok, value } -> produce(value, hd(functions), tl(functions))
+      { :error, error_tag } -> { :error, error_tag }
+    end
+  end
+end
